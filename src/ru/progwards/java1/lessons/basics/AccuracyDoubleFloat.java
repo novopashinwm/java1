f242f0e3d761b959c159c507b1f45a2b587670fe
5567 <novopashinwm@mail.ru>
2020-01-29 09:58:08 +0300

    Исправил косяки в классе AccuracyDoubleFloat

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -1,26 +1,27 @@
 package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
-    public final static double PId = 3.14;
-    public final static float PIf  = 3.14f;
-    public final static double Rd  = 6371.2;
-    public final static float  Rf  = 6371.2f;
+
+    public final static double R  = 6371.2;
+
 
     public static double volumeBallDouble(double radius) {
-        return 4.0 * PId * radius * radius * radius / 3.0;
+        return (4.0/3.0) * 3.14 * radius * radius * radius ;
     }
 
     public static float volumeBallFloat(float radius) {
-        return 4.0f * PIf * radius * radius * radius / 3.0f;
+        return (float) ((4f/3f) * 3.14f * radius * radius * radius);
     }
 
     public static double calculateAccuracy(double radius) {
-        return volumeBallDouble(radius) - volumeBallFloat((float) radius);
+        return volumeBallDouble(radius) - volumeBallFloat((float)radius);
     }
 
     public static void main(String[] args) {
-        System.out.println("volumeBallDouble=" + volumeBallDouble(Rd));
-        System.out.println("volumeBallFloat=" + volumeBallFloat(Rf));
-        System.out.println("calculateAccuracy=" + calculateAccuracy(Rd));
+        System.out.println("vd(1)=" + volumeBallDouble(1.0));
+        System.out.println("vf(1)=" + volumeBallFloat(1.0f));
+        System.out.println("volumeBallDouble=" + volumeBallDouble(R));
+        System.out.println("volumeBallFloat=" + volumeBallFloat((float)R));
+        System.out.println("calculateAccuracy=" + calculateAccuracy(R));
     }
 }
