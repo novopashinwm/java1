05844373093502dab405c9954deba4f935e4e513
5567 <novopashinwm@mail.ru>
2020-01-29 22:46:19 +0300

    Добавил функцию textGrade.

--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -7,4 +7,21 @@ public class Test1 {
     static int addAsStrings(int n1, int n2) {
         return Integer.parseInt(Integer.toString(n1) + Integer.toString(n2));
     }
+
+    static String textGrade(int grade) {
+        if (grade == 0) {
+            return "не оценено";
+        } else if (grade >= 1 && grade <= 20) {
+            return "очень плохо";
+        } else if (grade >= 21 && grade <= 40) {
+            return "плохо";
+        } else if (grade >= 41 && grade <= 60) {
+            return "удовлетворительно";
+        } else if (grade >= 61 && grade <= 80) {
+            return "хорошо";
+        } else if (grade >= 81 && grade <= 100) {
+            return "отлично";
+        }
+        return "не определено";
+    }
 }
