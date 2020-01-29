54078bd222085c2b3fb01036b0ef2c2e144471ff
5567 <novopashinwm@mail.ru>
2020-01-29 16:21:14 +0300

    Добавил функцию addAsStrings.

--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -4,4 +4,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
     }
+    static int addAsStrings(int n1, int n2) {
+        return Integer.parseInt(Integer.toString(n1) + Integer.toString(n2));
+    }
 }
