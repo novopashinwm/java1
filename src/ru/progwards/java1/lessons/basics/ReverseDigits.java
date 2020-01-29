fb280c0c50cf3093cf74f777928d0f40c2a23e21
5567 <novopashinwm@mail.ru>
2020-01-29 10:31:03 +0300

    Сделал в классе ReverseDigits переворачивание числа путем его деления и берения остатка от целого деления.

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -7,12 +7,15 @@ public class ReverseDigits {
      * @return
      */
     public static int reverseDigits(int number) {
-        StringBuilder sb = new StringBuilder(Integer.toString(number));
-        return Integer.parseInt(sb.reverse().toString());
+        //Буду считать, что у меня только 3 значные числа
+        int a = number / 100;
+        int b = number / 10  % 10;
+        int c = number % 10;
+        return c * 100 + b * 10 + a;
     }
 
     public static void main(String[] args) {
         System.out.println(reverseDigits(321));
-        System.out.println(reverseDigits(4321));
+        System.out.println(reverseDigits(421));
     }
 }
