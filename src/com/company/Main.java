0bfebeaa452000338b1f32c428193116224733d6
5567 <novopashinwm@mail.ru>
2020-01-29 08:18:24 +0300

    Hello world!

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte b1 = 123;
+        short s1 = 32023;
+        int i1 = 65432;
+        long l1 = 123456789012345L;
+        float fl = 1.22278f;
+        double pi = 3.1415926535;
+        double koe = 1.5e7;
+        String message = "Запись фильма \"Матрица\" находится в файле C:\\video\\matrix.avi";
+        System.out.println(1.53 % 1.0);
+
+    }
+}
