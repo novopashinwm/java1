ce41b2f36d141abc0ed8b71246d06f2c8ba74753
5567 <novopashinwm@mail.ru>
2020-01-31 06:49:12 +0300

    Добавил класс Point2D

---
+++ src/com/company/Point2D.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Point2D {
+    private int x, y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return x + ", " + y;
+    }
+}
