0da74c3c53ceb70c42271f7c2d1841eba0df8d92
5567 <novopashinwm@mail.ru>
2020-02-04 17:01:27 +0300

    Начал делать ДЗ 08

---
+++ src/com/company/Person.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Person {
+    public static class Child1{
+
+        public  String hello() {
+            return "привет";
+        }
+
+    }
+
+    public class Child2 {
+
+        public  String hello() {
+            return "servus";
+        }
+    }
+}
