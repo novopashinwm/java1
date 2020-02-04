3bc5683bbf1f8ee70ea087f4e13ee347d34ef8c0
5567 <novopashinwm@mail.ru>
2020-02-04 14:54:16 +0300

    Решение задач.

---
+++ src/ru/progwards/java1/lessons/test/Circle.java
@@ -0,0 +1,21 @@
+package ru.progwards.java1.lessons.test;
+
+import java.util.Objects;
+
+public class Circle {
+    double radius;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(circle.radius, radius) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+}
+
--- src/ru/progwards/java1/lessons/test/Rectangle.java
+++ src/ru/progwards/java1/lessons/test/Rectangle.java
@@ -1,8 +1,23 @@
 package ru.progwards.java1.lessons.test;
 
+import java.util.Objects;
+
 public class Rectangle {
     private double a;
     private double b;
+
+    @Override
+    public boolean equals(Object anObject) {
+        if (this == anObject) return true;
+        if (anObject == null || getClass() != anObject.getClass()) return false;
+        Rectangle rectangle = (Rectangle) anObject;
+        return rectangle.area() == this.area();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
 
     public Rectangle(double a, double b) {
         this.a = a;
@@ -20,5 +35,10 @@ public class Rectangle {
             return -1;
         }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Rectangle(2.0,3.0).equals(new Rectangle(3.0,2.0)));
+        System.out.println(new Rectangle(2.0,2.0).equals(new Rectangle(1.0,1.0)));
     }
 }
