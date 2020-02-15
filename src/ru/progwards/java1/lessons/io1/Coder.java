de0c49853859f60e3536521ef78f5a0b7b27a4b7
5567 <novopashinwm@mail.ru>
2020-02-15 13:31:43 +0300

    Сделал кодер.

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -1,7 +1,33 @@
 package ru.progwards.java1.lessons.io1;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Coder {
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
-        
+        try {
+            FileReader fileReader = new FileReader(inFileName);
+            FileWriter fileWriter = new FileWriter(outFileName);
+            Scanner scanner = new Scanner(fileReader);
+            String lineRead = "";
+            String lineWrite = "";
+            try {
+                while (scanner.hasNextLine()) {
+                    lineRead = scanner.nextLine();
+                    for (int i = 0; i < code.length; i++) {
+                        lineWrite += code[(int)lineRead.charAt(i)];
+                    }
+                    fileWriter.write(lineWrite);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                fileReader.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
