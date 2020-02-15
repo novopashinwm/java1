0b7c8b1a444d6a75967389b49be8ce7a03ad9887
5567 <novopashinwm@mail.ru>
2020-02-15 13:22:42 +0300

    Выполнил фильтрацию от символов.

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,8 +1,46 @@
 package ru.progwards.java1.lessons.io1;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class CharFilter {
-    public static void filterFile(String inFileName, String outFileName, String filter)
-    {
+    public static void filterFile(String inFileName, String outFileName, String filter) {
+        try {
+            FileReader fileReader = new FileReader(inFileName);
+            FileWriter fileWriter = new FileWriter(outFileName);
+            String lineRead =  "";
+            String lineWrite = "";
+
+            try {
+                Scanner scanner = new Scanner(fileReader);
+                while (scanner.hasNextLine()) {
+                    lineRead = scanner.nextLine();
+                    lineWrite = lineRead;
+                    for (int i = 0; i < filter.length(); i++) {
+                        lineWrite = lineWrite.replace(Character.toString(filter.charAt(i)),"");
+                    }
+                    fileWriter.write(lineWrite);
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            finally {
+                fileReader.close();
+                fileWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        filterFile("test01.txt","test02.txt"," -,.()");
+    }
+
+    public static void CreateTestFile() {
 
     }
 }
