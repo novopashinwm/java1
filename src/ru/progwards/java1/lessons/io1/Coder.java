d8dd86956e4a0fcdae02689bbeaacba53476f2a5
5567 <novopashinwm@mail.ru>
2020-02-15 22:23:17 +0300

    Исправил ошибку ДЗ 10.

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -3,6 +3,9 @@ package ru.progwards.java1.lessons.io1;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Coder {
@@ -13,22 +16,36 @@ public class Coder {
             Scanner scanner = new Scanner(fileReader);
             String lineRead = "";
             String lineWrite = "";
+            List<Character> list = new ArrayList<>();
             try {
                 while (scanner.hasNext()) {
-                    lineRead += scanner.next();
+                    lineRead += scanner.nextLine() + "\n";
+                }
+                lineWrite = lineRead;
+                for (int i = 0; i < 255; i++) {
+                    lineWrite = lineWrite.replace((char) i, code[i]);
                 }
 
-                for (int i = 0; i < code.length; i++) {
-                    lineWrite += code[(int)lineRead.charAt(i)];
-                }
+                System.out.println(Arrays.toString( list.toArray()));
                 fileWriter.write(lineWrite);
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                logName = e.getMessage();
             } finally {
                 fileReader.close();
+                fileWriter.close();
             }
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            logName = e.getMessage();
         }
+    }
+
+    public static void main(String[] args) {
+        char[] arr = new char[255];
+        for (int i = 0; i < 255; i++) {
+            arr[i] = (char) i;
+        }
+        arr[13] = 32;
+        arr[32] = 13;
+        Coder.codeFile("test01.txt", "test03.txt",arr, "");
     }
 }
--- src/ru/progwards/java1/lessons/test/Test2.java
+++ src/ru/progwards/java1/lessons/test/Test2.java
@@ -2,15 +2,7 @@ package ru.progwards.java1.lessons.test;
 
 public class Test2 {
     public static void main(String[] args) {
-        String str = null;
-        try {
-            System.out.println(str.toString());
-        }
-        catch (NullPointerException npe) {
-            System.out.println("catch-npe");
-        }
-        catch (Exception e) {
-            System.out.println("catch");
-        }
+        System.out.println((int)'J');
+        System.out.println((int) 'a');
     }
 }
