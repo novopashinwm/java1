9d6bf07b6a27c23bc1dd4d3120cf20e785277829
5567 <novopashinwm@mail.ru>
2020-02-18 22:18:16 +0300

    Сделал "заглушки" классов.

---
+++ src/ru/progwards/java1/lessons/register1/Counter.java
@@ -0,0 +1,12 @@
+package ru.progwards.java1.lessons.register1;
+
+public class Counter {
+
+    public static void inc(ByteRegister value) {
+
+    }
+
+    public static void dec(ByteRegister value) {
+
+    }
+}
---
+++ src/ru/progwards/java1/lessons/register1/Summator.java
@@ -0,0 +1,7 @@
+package ru.progwards.java1.lessons.register1;
+
+public class Summator {
+    public static boolean add(ByteRegister value1, ByteRegister value2) {
+        return false;
+    }
+}
