2a8ae69e9d29a929148d75036b3387698e981630
5567 <novopashinwm@mail.ru>
2020-02-22 22:43:24 +0300

    ДЗ 12

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -2,6 +2,7 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Creator {
@@ -15,15 +16,16 @@ public class Creator {
 
     public static Collection<Integer> fillOdd(int n) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0, p = 1; i < n*3; i++, p += 2) {
+        for (int i = 0, p = 1; i < n; i++, p += 2) {
             list.add(p);
         }
+        Collections.reverse(list);
         return list;
     }
 
     public static Collection<Integer> fill3(int n) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n*3; i +=3) {
             list.add(i);
             list.add(i*i);
             list.add(i*i*i);
