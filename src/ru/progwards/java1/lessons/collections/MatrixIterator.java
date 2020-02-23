9a52f1aa32e172726bab4f1074d1660ee182b955
5567 <novopashinwm@mail.ru>
2020-02-23 12:42:14 +0300

    Выполнил MatrixIterator

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -5,21 +5,35 @@ import java.util.Iterator;
 public class MatrixIterator<T> implements Iterator<T> {
 
     private T[][] array;
-    private int index = 0;
+    private int index = -1;
+    private int m = 0, n = 0;
 
 
     public MatrixIterator(T[][] array) {
         this.array = array;
+        this.m = array[0].length;
+        this.n = array.length;
 
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return (index + 1)< (m * n);
     }
 
     @Override
     public T next() {
-        return null;
+        index++;
+        return array[index/m][index % m];
+    }
+
+    public static void main(String[] args) {
+        Integer[][] arr = new Integer[][] { {1,2,3,4,5}
+                                  , {6,7,8,9,0}};
+        MatrixIterator<Integer> matrix = new MatrixIterator<Integer>(arr);
+
+        for ( ; matrix.hasNext() ; ) {
+            System.out.println(matrix.next());
+        }
     }
 }
