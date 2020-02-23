e83b97b965ac30f6ec6b8d9e326ba73ad0e5b9f5
5567 <novopashinwm@mail.ru>
2020-02-23 08:35:21 +0300

    Переделал Finder для тестирования.

--- src/ru/progwards/java1/lessons/collections/Finder.java
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -1,26 +1,41 @@
 package ru.progwards.java1.lessons.collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class Finder {
     //найти 2 соседних числа в коллекции сумма которых минимальна,
     //вернуть коллекцию, содержащую индексы этих чисел
     public static Collection<Integer> findMinSumPair(Collection<Integer> numbers) {
         int p1 = Integer.MAX_VALUE / 2, p2 = Integer.MAX_VALUE / 2;
-        return null;
+        List<Integer> list = new ArrayList<>();
+        return list;
     }
 
     //найти локальные максимумы - числа, которые больше соседа справа и слева.
     //Первый и последний элемент коллекции не может являться локальным  максимумом,
     // вернуть коллекцию, содержащую значения этих максимумов
     public static Collection<Integer> findLocalMax(Collection<Integer> numbers) {
-        return null;
+        List<Integer> list = new ArrayList<>();
+        return list;
     }
 
     //проверить, содержит ли коллекция все числа от 1 до size(),
     //порядок может быть произвольный
     public static boolean findSequence(Collection<Integer> numbers) {
-        return false;
+        boolean ret = true;
+        int i = 1;
+        for (Iterator<Integer> iterator = numbers.iterator() ; iterator.hasNext(); i++) {
+            Integer item = iterator.next();
+            if (item != i) {
+                ret = false;
+                break;
+            }
+        }
+
+        return ret;
     }
 
     //найдите максимальное количество повторяющихся подряд элементов.
@@ -29,7 +44,16 @@ public class Finder {
     //вернуть результат для элемента, повторяющаяся последовательность
     //которого началась с наименьшего индекса.
     public static String findSimilar(Collection<String> names) {
-        return null;
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 18; i++) {
+            list.add(i);
+        }
+        System.out.println(Finder.findSequence( list));
     }
 
 }
