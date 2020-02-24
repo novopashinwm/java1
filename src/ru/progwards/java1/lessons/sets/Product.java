000a63a84161e5e6bd78d18039a57f1a0f9caf13
5567 <novopashinwm@mail.ru>
2020-02-24 17:53:02 +0300

    Реализовал аналитику с products.

--- src/ru/progwards/java1/lessons/sets/Product.java
+++ src/ru/progwards/java1/lessons/sets/Product.java
@@ -1,5 +1,7 @@
 package ru.progwards.java1.lessons.sets;
 
+import java.util.Objects;
+
 public class Product {
     private String code;
 
@@ -9,5 +11,18 @@ public class Product {
 
     public String getCode() {
         return code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(code, product.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
     }
 }
--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -1,8 +1,6 @@
 package ru.progwards.java1.lessons.sets;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ProductAnalytics {
     private List<Product> products;
@@ -15,21 +13,42 @@ public class ProductAnalytics {
 
     public Set<Product> existInAll() {
         Set<Product> set = new HashSet<>();
+        Iterator<Shop> it = shops.iterator();
+        set.addAll(it.next().getProducts());
+        for (; it.hasNext(); ) {
+            set.retainAll(it.next().getProducts());
+        }
         return set;
     }
 
     public Set<Product> existAtListInOne() {
         Set<Product> set = new HashSet<>();
+        for (Iterator<Shop> it = shops.iterator(); it.hasNext();) {
+            set.addAll(it.next().getProducts());
+        }
         return set;
     }
 
     public Set<Product> notExistInShops() {
-        Set<Product> set = new HashSet<>();
+        Set<Product> set = new HashSet<>(products);
+        for (Iterator<Shop> it = shops.iterator(); it.hasNext();) {
+            set.removeAll(it.next().getProducts());
+        }
         return set;
     }
 
     public Set<Product> existOnlyInOne(){
-        Set<Product> set = new HashSet<>();
+        Set<Product> set = existAtListInOne();
+        set.removeAll(existInAll());
         return set;
+    }
+
+    public static void main(String[] args) {
+        Shop shop1 = new Shop(Arrays.asList(new Product("p01"), new Product("p02")));
+        Shop shop2 = new Shop(Arrays.asList(new Product("p02"), new Product("p03")));
+        ProductAnalytics pa = new ProductAnalytics(
+                Arrays.asList(new Product("p01"), new Product("p02"), new Product("p14")
+                , new Product("p03")), Arrays.asList(shop1, shop2));
+        System.out.println(pa.existOnlyInOne());
     }
 }
