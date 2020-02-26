9ffeaf8b47f4739b54b0a008b36cc5cc93f7cd75
5567 <novopashinwm@mail.ru>
2020-02-26 08:32:45 +0300

    Доделал чтение файла.

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -1,10 +1,35 @@
 package ru.progwards.java1.lessons.sets;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 public class LettersInFile {
-    public static String process(String fileName) {
+    public static String process(String fileName) throws IOException {
         TreeSet<Character> set = new TreeSet<>();
-        return "";
+        try (Reader reader = new FileReader(fileName)) {
+            Scanner scanner = new Scanner(reader);
+            while (scanner.hasNext()) {
+                char[] arrCh = scanner.next().toCharArray();
+                for (int i = 0; i < arrCh.length; i++) {
+                    set.add(arrCh[i]);
+                }
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+
+        return set.toString();
+    }
+
+    public static void main(String[] args) {
+        try {
+            process("test02.txt");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
