d5ec7ca7d7b847953564fde37b2646f9046cf678
5567 <novopashinwm@mail.ru>
2020-02-26 08:15:10 +0300

    Доработал домашнее задание.

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -38,17 +38,39 @@ public class ProductAnalytics {
     }
 
     public Set<Product> existOnlyInOne(){
-        Set<Product> set = existAtListInOne();
-        set.removeAll(existInAll());
+        Set<Product> set = new HashSet<>();
+        for (Iterator<Shop> it = shops.iterator(); it.hasNext();) {
+            for (Iterator<Product> p = it.next().getProducts().iterator(); p.hasNext() ; ) {
+                Product product = p.next();
+                if (!set.contains(product))
+                    set.add(product);
+                else
+                    set.remove(product);
+            }
+        }
         return set;
     }
 
+    public static List<Product> getListProductfromArray(String[] arr) {
+        List<Product> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(new Product(arr[i]));
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
-        Shop shop1 = new Shop(Arrays.asList(new Product("p01"), new Product("p02")));
-        Shop shop2 = new Shop(Arrays.asList(new Product("p02"), new Product("p03")));
+        String[] prShop1 = new String[] { "art-2" , "art-4" ,"art-7" };
+        String[] prShop2 = new String[] { "art-1" , "art-5" , "art-10" };
+        String[] prShop3 = new String[] { "art-1" , "art-4" ,"art-5" , "art-9" , "art-10"};
+        String[] prProds = new String[] { "art-1" , "art-2" , "art-3" ,"art-4" , "art-5"
+                , "art-6" ,"art-7" , "art-8" , "art-9" , "art-10"};
+
+        Shop shop1 = new Shop(getListProductfromArray(prShop1));
+        Shop shop2 = new Shop(getListProductfromArray(prShop2));
+        Shop shop3 = new Shop(getListProductfromArray(prShop3));
         ProductAnalytics pa = new ProductAnalytics(
-                Arrays.asList(new Product("p01"), new Product("p02"), new Product("p14")
-                , new Product("p03")), Arrays.asList(shop1, shop2));
+                getListProductfromArray(prProds), Arrays.asList(shop1, shop2, shop3));
         System.out.println(pa.existOnlyInOne());
     }
 }
