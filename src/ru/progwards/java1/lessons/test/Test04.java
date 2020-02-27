220ec7ec267eeae33de440ad2453fe5fbf9e689b
5567 <novopashinwm@mail.ru>
2020-02-27 23:21:24 +0300

    Проверка тестовых примеров.

--- src/ru/progwards/java1/lessons/test/Test04.java
+++ src/ru/progwards/java1/lessons/test/Test04.java
@@ -1,8 +1,25 @@
 package ru.progwards.java1.lessons.test;
 
+import java.util.ArrayDeque;
+
 public class Test04 {
     public static void main(String[] args) {
-        double d = 3.0e300;
-        float  f = (float) d;
+        ArrayDeque deque = new ArrayDeque<>();
+
+        for (int i = 0; i <= 10; i++) {
+            deque.offer(i);
+            if (i%2 == 0)
+                deque.poll();
+        }
+
+        System.out.println(deque);
+    }
+
+    ArrayDeque<Integer> array2queue(int[] a) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < a.length; i++) {
+            deque.offer(a[i]);
+        }
+        return deque;
     }
 }
