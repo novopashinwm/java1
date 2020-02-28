69e02a73fb36e13be830573c2725651a2ae1cd3e
5567 <novopashinwm@mail.ru>
2020-02-28 23:38:49 +0300

    Исправил сдвиг влево.

--- src/ru/progwards/java1/lessons/register1/ShiftRegister.java
+++ src/ru/progwards/java1/lessons/register1/ShiftRegister.java
@@ -2,7 +2,8 @@ package ru.progwards.java1.lessons.register1;
 
 public class ShiftRegister {
     public static void right(ByteRegister value) {
-        ByteRegister v1 = new ByteRegister(Byte.parseByte(value.toDecString()));
+
+        ByteRegister v1 = new ByteRegister((byte) Integer.parseInt(value.toDecString()));
         value.bitArr[value.bitArr.length-1] = value.b0;
         for (int i = value.bitArr.length-2; i >=0 ; i--) {
             value.bitArr[i] = v1.bitArr[i+1];
@@ -12,7 +13,7 @@ public class ShiftRegister {
     public static void left(ByteRegister value) {
         ByteRegister v1 = new ByteRegister(Byte.parseByte(value.toDecString()));
         value.bitArr[0] = value.b0;
-        for (int i = 1; i <value.bitArr.length-1 ; i++) {
+        for (int i = 1; i <value.bitArr.length ; i++) {
             value.bitArr[i] = v1.bitArr[i-1];
         }
     }
@@ -21,7 +22,7 @@ public class ShiftRegister {
         int i = 1;
         i >>=1;
         System.out.println(i);
-        ByteRegister byteRegister = new ByteRegister((byte)4);
+        ByteRegister byteRegister = new ByteRegister((byte)64);
         ShiftRegister.left(byteRegister);
         System.out.println(byteRegister.toDecString());
         ShiftRegister.right(byteRegister);
