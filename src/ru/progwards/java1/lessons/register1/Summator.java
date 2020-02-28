c92c508b593de4c6198cec7e851a6a38d5ffb82e
5567 <novopashinwm@mail.ru>
2020-02-28 23:53:54 +0300

    Поробывал написать битовый сумматор.

--- src/ru/progwards/java1/lessons/register1/Summator.java
+++ src/ru/progwards/java1/lessons/register1/Summator.java
@@ -2,6 +2,26 @@ package ru.progwards.java1.lessons.register1;
 
 public class Summator {
     public static boolean add(ByteRegister value1, ByteRegister value2) {
-        return false;
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            if (value1.bitArr[i].equals(value1.b0) && value2.bitArr[i].equals(value2.b1)) {
+                value1.bitArr[i].equals(value1.b1);
+                if (count>0)
+                    count++;
+            } else if (value1.bitArr[i].equals(value1.b1) && value2.bitArr[i].equals(value2.b1)) {
+                count++;
+            } else if (value1.bitArr[i].equals(value1.b0) && value2.bitArr[i].equals(value2.b0)) {
+                if (count>0) {
+                    count--;
+                    value1.bitArr[i] = value1.b1;
+                }
+            } else if (value1.bitArr[i].equals(value1.b1) && value2.bitArr[i].equals(value2.b0)) {
+                if (count>0) {
+                    count++;
+                    value1.bitArr[i] = value1.b0;
+                }
+            }
+        }
+        return count==0;
     }
 }
