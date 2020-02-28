9c33a99badbb4f0e175e139b6cb0736f4c1a423b
5567 <novopashinwm@mail.ru>
2020-02-28 23:32:02 +0300

    Доделал дополнительное задание 5.1.

--- src/ru/progwards/java1/lessons/register1/ShiftRegister.java
+++ src/ru/progwards/java1/lessons/register1/ShiftRegister.java
@@ -1,19 +1,30 @@
 package ru.progwards.java1.lessons.register1;
 
 public class ShiftRegister {
-    public static void left(ByteRegister value) {
+    public static void right(ByteRegister value) {
         ByteRegister v1 = new ByteRegister(Byte.parseByte(value.toDecString()));
         value.bitArr[value.bitArr.length-1] = value.b0;
-        for (int i = value.bitArr.length-2; i >=0 ; i++) {
+        for (int i = value.bitArr.length-2; i >=0 ; i--) {
             value.bitArr[i] = v1.bitArr[i+1];
         }
     }
 
-    public static void right(ByteRegister value) {
+    public static void left(ByteRegister value) {
         ByteRegister v1 = new ByteRegister(Byte.parseByte(value.toDecString()));
         value.bitArr[0] = value.b0;
-        for (int i = 1; i <value.bitArr.length ; i++) {
+        for (int i = 1; i <value.bitArr.length-1 ; i++) {
             value.bitArr[i] = v1.bitArr[i-1];
         }
+    }
+
+    public static void main(String[] args) {
+        int i = 1;
+        i >>=1;
+        System.out.println(i);
+        ByteRegister byteRegister = new ByteRegister((byte)4);
+        ShiftRegister.left(byteRegister);
+        System.out.println(byteRegister.toDecString());
+        ShiftRegister.right(byteRegister);
+        System.out.println(byteRegister.toDecString());
     }
 }
