4716e45823a2ab3c21978a9a80012d468097849c
5567 <novopashinwm@mail.ru>
2020-02-29 18:23:38 +0300

    Решение задачи со стеком.

--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -1,38 +1,61 @@
 package ru.progwards.java1.lessons.queues;
 
-import java.util.List;
+import java.util.Stack;
 
 public class StackCalc {
+
+    Stack<Double> stack = new Stack<>();
 
     public static Calculate calculate;
 
     public void push(double value) {
-
+        stack.push(value);
     }
 
     public double pop() {
-        return 0.0;
+        if (!stack.empty())
+            return stack.pop();
+        return 0;
     }
 
     public void add() {
-
+        double a = pop();
+        double b = pop();
+        push(a+b);
     }
 
     public void sub() {
-
+        double a = pop();
+        double b = pop();
+        push( a - b);
     }
 
     public void mul() {
-
+        double a = pop();
+        double b = pop();
+        push( a * b);
     }
 
     public void div() {
+        double a = pop();
+        double b = pop();
+        push( a / b);
+    }
 
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        int i = stack.peek();
     }
 
     static class  Calculate {
         public static double calculation1() {
-            return 0.0;
+            StackCalc calc = new StackCalc();
+            calc.push(12.1);
+            calc.push(3.0);
+            calc.add();
+            calc.push(2.2);
+            calc.mul();
+            return calc.pop();
         }
         public static double calculation2() {
             return 0.0;
---
+++ src/ru/progwards/java1/lessons/queues/TestClass3.java
@@ -0,0 +1,5 @@
+package ru.progwards.java1.lessons.queues;
+
+public class TestClass3 {
+    StackCalc.Calculate calculate;
+}
