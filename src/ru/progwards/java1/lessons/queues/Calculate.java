06cc75c2d805eee26cf94495b393514df334f078
5567 <novopashinwm@mail.ru>
2020-02-29 19:03:21 +0300

    Исправление ошибок в стек классе, по вычитанию и делению, чтобы прошли тесты.

--- src/ru/progwards/java1/lessons/queues/Calculate.java
+++ src/ru/progwards/java1/lessons/queues/Calculate.java
@@ -13,22 +13,23 @@ public class  Calculate {
     public static double calculation2() {
         //(737.22+24)/(55.6-12.1)+(19-3.33)*(87+2*(13.001-9.2))
         StackCalc stackCalc = new StackCalc();
-        stackCalc.push(12.1);
-        stackCalc.push(55.6);
-        stackCalc.sub();
+
         stackCalc.push(24.0);
         stackCalc.push(737.22);
         stackCalc.add();
+        stackCalc.push(55.6);
+        stackCalc.push(12.1);
+        stackCalc.sub();
         stackCalc.div();
-        stackCalc.push(9.2);
         stackCalc.push(13.001);
+        stackCalc.push(9.2);
         stackCalc.sub();
         stackCalc.push(2.0);
         stackCalc.mul();
         stackCalc.push(87.0);
         stackCalc.add();
-        stackCalc.push(3.33);
         stackCalc.push(19.0);
+        stackCalc.push(3.33);
         stackCalc.sub();
         stackCalc.mul();
         stackCalc.add();
