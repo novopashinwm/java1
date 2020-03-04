fe16b51b7eed9ec881b91471e1dae171035b1a2d
5567 <novopashinwm@mail.ru>
2020-03-04 20:47:52 +0300

    Поправил Фибо и не совсем понял как делать SalesInfo.

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -7,50 +7,43 @@ import java.util.Map;
 public class FiboMapCache {
     private Map<Integer, BigDecimal> fiboCache = null;
     private boolean casheOn = false;
-    private BigDecimal a = BigDecimal.ZERO;
-    private BigDecimal b = BigDecimal.ONE;
-    private BigDecimal x = null;
-    private BigDecimal ret = BigDecimal.ZERO;
 
     public FiboMapCache(boolean cacheOn) {
         this.casheOn = cacheOn;
-        if (casheOn) {
-            InitCache();
-        }
     }
 
     public BigDecimal fiboNumber(int n) {
-
-        int begin = 2;
-
         if (casheOn) {
-            if (fiboCache == null) {
-                InitCache();
-            }
-            ret = fiboCache.get(n);
-            if (ret != null) {
+            return fidoNumberWithCash(n);
+        } else  {
+            return fidoNumberNonCash(n);
+        }
+    }
+
+    private BigDecimal fidoNumberWithCash(int n) {
+        int begin = 2;
+        BigDecimal ret = BigDecimal.ONE;
+        BigDecimal a = BigDecimal.ZERO;
+        BigDecimal b = BigDecimal.ONE;
+
+        if (fiboCache != null ) {
+            begin = fiboCache.size();
+            if ((ret = fiboCache.get(n)) != null) {
                 return ret;
             }
-            begin = fiboCache.size();
-        }
-
-        if (!casheOn)  {
-            a = BigDecimal.ZERO;
-            b = BigDecimal.ONE;
-            ret = BigDecimal.ONE;
+        } else  {
+            fiboCache = new HashMap<>();
+            fiboCache.put(1, BigDecimal.ONE);
+            fiboCache.put(2, BigDecimal.ONE);
         }
 
         for (int i = begin; i <=n ; i++) {
-            if (casheOn &&  (x = fiboCache.get(i))!= null) {
+            if ( (ret = fiboCache.get(i))!= null) {
                 a = fiboCache.get(i-2);
                 b = fiboCache.get(i-1);
-                ret = x;
             } else {
-
                 ret = a.add(b);
-                if (casheOn) {
-                    fiboCache.put(i, ret);
-                }
+                fiboCache.put(i, ret);
             }
             a = b;
             b = ret;
@@ -58,18 +51,23 @@ public class FiboMapCache {
         return ret;
     }
 
+    private BigDecimal fidoNumberNonCash(int n) {
+        BigDecimal a = BigDecimal.ZERO;
+        BigDecimal b = BigDecimal.ONE;
+        BigDecimal x = BigDecimal.ONE;
+        for (int i = 2; i <=n ; i++) {
+            x = a.add(b);
+            a = b;
+            b = x;
+        }
+        return x;
+
+    }
+
     public void clearCahe() {
         fiboCache = null;
     }
 
-    private void InitCache() {
-        fiboCache = new HashMap<>();
-        a = BigDecimal.ZERO;
-        b = BigDecimal.ONE;
-        fiboCache.put(0, a);
-        fiboCache.put(1, b);
-        ret = BigDecimal.ZERO;
-    }
 
     public static void test() {
         FiboMapCache f = new FiboMapCache(false);
@@ -107,8 +105,19 @@ public class FiboMapCache {
     //1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,
     // 1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229
     public static void main(String[] args) {
+        FiboMapCache.test();
        FiboMapCache f = new FiboMapCache(true);
-        f.test();
+        for (int i = 1; i < 30; i++) {
+            System.out.print(f.fiboNumber(i) + ", ");
+        }
+
+
+        System.out.println();
+        f = new FiboMapCache(false);
+        for (int i = 1; i < 30; i++) {
+            System.out.print(f.fiboNumber(i) + ", ");
+        }
+        System.out.println();
 
     }
 }
--- src/ru/progwards/java1/lessons/maps/SalesInfo.java
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -1,9 +1,63 @@
 package ru.progwards.java1.lessons.maps;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.*;
+
 public class SalesInfo {
     StringBuilder sb = new StringBuilder();
 
     public int loadOrders(String fileName) {
-        return 1;
+        int cnt = 0;
+        try (Reader reader = new FileReader(fileName)) {
+            Scanner scanner = new Scanner(reader);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] arrS = line.split("\\,");
+                if (arrS.length==4  && arrS[3].trim().matches("^[0-9]+$")  ) {
+                    sb.append(line + "\n");
+                    cnt++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return cnt;
+    }
+    public Map<String, Double> getGoods() {
+        TreeMap<String, Double> map = new TreeMap<>();
+        String[] arrS = sb.toString().split("\n");
+        for (int i = 0; i < arrS.length; i++) {
+            String[] item = arrS[i].split("\\,");
+            if (map.containsKey(item[1].trim())) {
+                map.put(item[1].trim(), map.get(item[1].trim()) +
+                        Double.parseDouble(item[2].trim()) * Integer.parseInt(item[3].trim()));
+            }
+            else {
+                map.put(item[1].trim(), Double.parseDouble(item[2].trim()) * Integer.parseInt(item[3].trim()));
+            }
+        }
+        return map;
+    }
+
+    public Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers() {
+        Map<String, AbstractMap.SimpleEntry<Double, Integer>> map = new TreeMap<>();
+        String[] arrS = sb.toString().split("\n");
+        for (int i = 0; i < arrS.length; i++) {
+            String[] item = arrS[i].split("\\,");
+            if (map.containsKey(item[1].trim())) {
+                Map<Double, Integer>  m = new HashMap<>();
+                m.put(Double.parseDouble(item[2].trim()) , Integer.parseInt(item[3].trim()));
+
+            }
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        SalesInfo salesInfo = new SalesInfo();
+        System.out.println(salesInfo.loadOrders("test07.txt"));
+        System.out.println(salesInfo.getGoods());
     }
 }
