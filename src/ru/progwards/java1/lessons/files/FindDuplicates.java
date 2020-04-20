a79b2c0a1f35ef717c0898719868a14e3a7ce0f1
novopashinwm <novopashinwm@mail.ru>
2020-04-20 20:21:19 +0300

    Тесты для 17 задания.

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -1,12 +1,93 @@
 package ru.progwards.java1.lessons.files;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+/*
+Задача 1, класс FindDuplicates: не пройдено, оценка: 0.0
+Комментарий:
+ERROR: Тест "Метод findDuplicates(String startPath)" не пройден. Метод работает неправльно.
+В целевом каталоге находятся файлы file1 и file2.
+Идентичные им файлы находятся в подкаталогах file1: dir1 и dir2/dir3, file2: dir2.
+В подкаталоге dir1 находится файл file3, идентичный которому файл находится в подкаталоге dir2/dir3.
+В подкаталоге dir2 находится файл file1, который отличается от ранее упомянутого файла file1 только содержимым, но не размером и временем.
+В подкаталоге dir1 находится файл file2, который отличается от ранее упомянутого файла file2 только временем.
+Возвращены списки строк, содержащие:
+Ожидалось:
+dir2/file2, file2
+dir1/file1, dir2/dir3/file1
+dir1/file3, dir2/dir3/file3
+* */
 public class FindDuplicates {
-    public List<List<String>> findDuplicates(String startPath) {
+    //    Создание Map с именами файлов по результатам сравнения файлов
+    private List<Path> fileList(String start){
+        List<Path> fileList = new ArrayList<>();
+        Path pathFile = Paths.get(start);
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/**");
+        try {
+            Files.walkFileTree(pathFile, new SimpleFileVisitor<Path>() {
 
-        List<List<String>> list = new ArrayList<>();
-        return list;
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+//                    if (pathMatcher.matches(pathFile.relativize(path))) {
+                    if (pathMatcher.matches(path)) {
+                        fileList.add(path);
+                        return FileVisitResult.CONTINUE;
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileList;
     }
+
+
+    // сравнение файлов по атрибутам и содержимому
+    private boolean fullFiltr(Path file1, Path file2){
+        try {
+            if (Files.getAttribute(file1, "lastModifiedTime").equals(Files.getAttribute(file2,
+                    "lastModifiedTime")) &&
+                    Files.getAttribute(file1, "size").equals(Files.getAttribute(file2, "size"))) {
+                if (Arrays.equals(Files.readAllBytes(file1), Files.readAllBytes(file2))) {
+//                    System.out.println("Сошлись)");
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    //    обработка Map и вывод результата
+    public List<List<String>> findDuplicates(String startPath){
+        List<List<String>> result = new ArrayList<>();
+        List<Path> tmpFileList = fileList(startPath);
+
+        for (int i = 0; i < tmpFileList.size(); i++) {
+            Path file1 = tmpFileList.get(i);
+            List<String> filePathList = new ArrayList<>();
+            for (int f = i+1; f < tmpFileList.size(); f++) {
+                Path file2 = tmpFileList.get(f);
+                if (file1.getFileName().compareTo(file2.getFileName()) == 0 && fullFiltr(file1, file2)) {
+                    filePathList.add(file1.toString());
+                    filePathList.add(file2.toString());
+                    tmpFileList.remove(f);
+                }
+            }
+            result.add(filePathList);
+        }
+        return result;
+    }
+
 }
--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -1,37 +1,214 @@
 package ru.progwards.java1.lessons.files;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
 
 public class OrderProcessor {
     private String startPath;
+    private int failedFileNumber;
+    private Map<String, List<Order>> ordersByShops; // key: shopId, value: ordersList
 
-    public OrderProcessor(String startPath)
-    {
-
+    public OrderProcessor(String startPath) {
+        this.startPath = startPath;
+        this.failedFileNumber = 0;
+        this.ordersByShops = new HashMap<>();
     }
 
+    //    загружает заказы за указанный диапазон дат, с start до finish, обе даты включительно.
+//    Если start == null, значит нет ограничения по дате слева, если finish == null,
+//    значит нет ограничения по дате справа, если shopId == null - грузим для всех магазинов
+//    При наличии хотя бы одной ошибке в формате файла, файл полностью игнорируется, т.е. Не поступает в обработку.
+//    Метод возвращает количество файлов с ошибками. При этом, если в классе содержалась информация, ее надо удалить
     public int loadOrders(LocalDate start, LocalDate finish, String shopId) {
-        return 1;
+
+        LocalDate startDate = (start == null) ? LocalDate.of(1970, 1, 1) : start;// Почему IDE требует создавать новую
+        // переменную startDate и не позволяет использовать параметр start, передаваемый в функцию ???
+
+        LocalDate finishDate = (finish == null) ? LocalDate.now() : finish; // Почему IDE требует создавать новую
+        // переменную finishDate и не позволяет использовать параметр finish, передаваемый в функцию ???
+
+        if (shopId == null)
+            shopId = "";
+
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/" + shopId + "*.csv");
+        try {
+            Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (pathMatcher.matches(file)) {
+
+                        LocalDateTime lastDateTime = LocalDateTime
+                                .ofInstant(attrs.lastModifiedTime().toInstant(), ZoneId.systemDefault());
+
+                        if (LocalDate.from(lastDateTime).isBefore(startDate)
+                                || LocalDate.from(lastDateTime).isAfter(finishDate)) {
+                            return FileVisitResult.CONTINUE;
+                        }
+
+                        String fileName = file.getFileName().toString();
+                        String[] idsArr = fileName.split("-");
+                        if (idsArr.length != 3) {
+                            failedFileNumber++;
+                            return FileVisitResult.CONTINUE;
+                        }
+                        String shopId = idsArr[0];
+                        String orderId = idsArr[1];
+                        String customerId = idsArr[2].split("\\.")[0];
+                        if (shopId.length() != 3
+                                || orderId.length() != 6
+                                || customerId.length() != 4) {
+                            failedFileNumber++;
+                            return FileVisitResult.CONTINUE;
+                        }
+                        Order order = new Order();
+                        order.shopId = shopId;
+                        order.orderId = orderId;
+                        order.customerId = customerId;
+                        order.datetime = lastDateTime;
+                        order.items = new ArrayList<>();
+                        order.sum = 0;
+
+                        List<String> goods = Files.readAllLines(file);
+                        for (String goodStr : goods
+                        ) {
+                            String[] strArr = goodStr.trim().split("\\s*,\\s*");
+
+                            if (strArr.length != 3) {
+                                failedFileNumber++;
+                                return FileVisitResult.CONTINUE;
+                            }
+
+                            for (char ch : strArr[1].toCharArray()
+                            ) {
+                                if (!Character.isDigit(ch)) {
+                                    failedFileNumber++;
+                                    return FileVisitResult.CONTINUE;
+                                }
+                            }
+
+                            int decimalSeparatorCounter = 0;
+                            for (char ch : strArr[2].toCharArray()
+                            ) {
+                                if (Character.isDigit(ch))
+                                    continue;
+
+                                if (String.valueOf(ch).equals(".")) {
+                                    decimalSeparatorCounter++;
+                                    if (decimalSeparatorCounter > 1) {
+                                        failedFileNumber++;
+                                        return FileVisitResult.CONTINUE;
+                                    }
+                                } else {
+                                    failedFileNumber++;
+                                    return FileVisitResult.CONTINUE;
+                                }
+                            }
+
+                            int goodsCount = Integer.parseInt(strArr[1]);
+                            double oneGoodPrice = Double.parseDouble(strArr[2]);
+
+                            OrderItem orderItem = new OrderItem();
+                            orderItem.count = goodsCount;
+                            orderItem.price = oneGoodPrice;
+                            orderItem.googsName = strArr[0];
+
+                            order.items.add(orderItem);
+                            order.sum += (goodsCount * oneGoodPrice);
+                        }
+                        order.items.sort(new Comparator<OrderItem>() {
+                            @Override
+                            public int compare(OrderItem orderItem1, OrderItem orderItem2) {
+                                return orderItem1.googsName.compareTo(orderItem2.googsName);
+                            }
+                        });
+
+                        ordersByShops.putIfAbsent(order.shopId, new ArrayList<>());
+                        ordersByShops.get(order.shopId).add(order);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return failedFileNumber;
     }
 
-    public List<Order> process(String shopId){
-        return new ArrayList<>();
+    public List<Order> process(String shopId) {
+        List<Order> ordersList = new ArrayList<>();
+        if (shopId == null) {
+            for (List<Order> oneShopOrders : ordersByShops.values()
+            ) {
+                ordersList.addAll(oneShopOrders);
+            }
+        } else {
+            if (ordersByShops.containsKey(shopId))
+                ordersList.addAll(ordersByShops.get(shopId));
+        }
+        ordersList.sort(new Comparator<Order>() {
+            @Override
+            public int compare(Order o1, Order o2) {
+                return o1.datetime.compareTo(o2.datetime);
+            }
+        });
+        return ordersList;
     }
 
-    public Map<String, Double> statisticsByShop()
-    {
-        return new HashMap<>();
+    public Map<String, Double> statisticsByShop() {
+        SortedMap<String, Double> statisticByShops = new TreeMap<>();
+        for (String shopId : ordersByShops.keySet()
+        ) {
+            double sum = 0.0;
+            for (Order order : ordersByShops.get(shopId)
+            ) {
+                sum += order.sum;
+            }
+            statisticByShops.put(shopId, sum);
+        }
+        return statisticByShops;
     }
 
-    public Map<String, Double> statisticsByGoods()
-    {
-        return new HashMap<>();
+    public Map<String, Double> statisticsByGoods() {
+        SortedMap<String, Double> statisticByGoods = new TreeMap<>();
+        for (String shopId : ordersByShops.keySet()
+        ) {
+            for (Order order : ordersByShops.get(shopId)
+            ) {
+                for (OrderItem orderItem : order.items
+                ) {
+                    statisticByGoods.putIfAbsent(orderItem.googsName, 0.0);
+                    double sum = statisticByGoods.get(orderItem.googsName) + orderItem.price * (double) orderItem.count;
+                    statisticByGoods.put(orderItem.googsName, sum);
+                }
+            }
+        }
+        return statisticByGoods;
     }
+
     public Map<LocalDate, Double> statisticsByDay() {
-        return new HashMap<>();
+        SortedMap<LocalDate, Double> statisticByDays = new TreeMap<>();
+        for (String shopId : ordersByShops.keySet()
+        ) {
+            for (Order order : ordersByShops.get(shopId)
+            ) {
+                LocalDate date = LocalDate.from(order.datetime);
+                statisticByDays.putIfAbsent(date, 0.0);
+                double sum = statisticByDays.get(date) + order.sum;
+                statisticByDays.put(date, sum);
+            }
+        }
+        return statisticByDays;
     }
 }
