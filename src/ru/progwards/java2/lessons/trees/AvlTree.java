7275ce1fd21262b854a2a67db94e34639fe0087b
novopashinwm <novopashinwm@mail.ru>
2020-07-25 16:50:59 +0300

    Выполнена домашка по деревьям.

--- src/ru/progwards/java2/lessons/trees/AvlTree.java
+++ src/ru/progwards/java2/lessons/trees/AvlTree.java
@@ -1,5 +1,10 @@
 package ru.progwards.java2.lessons.trees;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class AvlTree<K extends Comparable<K>, V>  {
@@ -7,12 +12,12 @@ public class AvlTree<K extends Comparable<K>, V>  {
     private static final String KEYEXIST = "Key already exist";
     private static final String KEYNOTEXIST = "Key not exist";
 
-    private AvlLeaf root;
-
+    public AvlLeaf root;
+/*
     class AvlLeaf<K extends Comparable<K>, V> {
         K key;
         V value;
-        AvlLeaf parent;
+        AvlLeaf<K, V> parent;
         AvlLeaf left;
         AvlLeaf right;
         private int height;
@@ -21,6 +26,19 @@ public class AvlTree<K extends Comparable<K>, V>  {
             this.key = key;
             this.value = value;
             height= 1;
+        }
+
+        void add(AvlLeaf<K, V> leaf) throws TreeException {
+            int cmp = leaf.key.compareTo(key);
+            if (cmp == 0)
+                throw new TreeException(KEYEXIST);
+            if (cmp > 0) {
+                right = leaf;
+                leaf.parent = this;
+            } else {
+                left = leaf;
+                leaf.parent = this;
+            }
         }
 
 
@@ -52,42 +70,48 @@ public class AvlTree<K extends Comparable<K>, V>  {
                 right.process(consumer);
         }
     }
+*/
+    static class AvlLeaf <K extends Comparable<K>, V> {
+        private AvlLeaf<K , V> left, right, parent;
+        private int height = 1;
+        private K key;
+        private V value;
+
+        private AvlLeaf (K key, V val) {
+            this.key = key;
+            this.value = val;
+        }
+
+        public void process(Consumer<AvlLeaf<K,V>> consumer) {
+            if (left != null)
+                left.process(consumer);
+            consumer.accept(this);
+            if (right != null)
+                right.process(consumer);
+        }
+
+        private AvlLeaf<K,V> find(K key) {
+            int cmp = key.compareTo(this.key);
+            if (cmp > 0)
+                if (right != null)
+                    return right.find(key);
+                else
+                    return this;
+            if (cmp < 0)
+                if (left != null)
+                    return left.find(key);
+                else
+                    return this;
+            return this;
+        }
+
+        public String toString() {
+            return "("+key+","+value+")";
+        }
+    }
 
     private int getHeight(AvlLeaf leaf) {
-        if (leaf == null) return 0;
-        return leaf.height;
-    }
-
-    private AvlLeaf rightRotate(AvlLeaf y) {
-        AvlLeaf x = y.left;
-        AvlLeaf temp = x.right;
-
-        // Perform rotation
-        x.right = y;
-        y.left = temp;
-
-        // Update heights
-        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
-        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
-
-        // Return new root
-        return x;
-    }
-
-    private AvlLeaf leftRotate(AvlLeaf x)
-    {
-        AvlLeaf y = x.right;
-        AvlLeaf temp = y.left;
-
-        //Rotation
-        y.left = x;
-        x.right = temp;
-
-        //update heights
-        x.height = Math.max(getHeight(x.left), getHeight(x.right)) +1;
-        y.height = Math.max(getHeight(y.left), getHeight(y.right)) +1;
-
-        return y;
+        return (leaf== null) ? 0 : leaf.height;
     }
 
     private int getBalance(AvlLeaf leaf)
@@ -96,46 +120,86 @@ public class AvlTree<K extends Comparable<K>, V>  {
         return getHeight(leaf.left) - getHeight(leaf.right);
     }
 
-    public void put(K key, V value) {
-       root = insert(root, key, value);
+    public void put(K key, V value) throws TreeException {
+        root = insert(root, key, value);
+        updateHeight(root);
     }
 
-    AvlLeaf insert (AvlLeaf node, K key, V value) {
-        if (node == null)
-            return new AvlLeaf(key,value);
-        int cmp = key.compareTo((K)node.key);
-        if (cmp <0)
-            node.left = insert(node.left, key,value);
-        else if (cmp>0)
-            node.right = insert(node.right, key, value);
+    public AvlLeaf insert(AvlLeaf<K, V> node, K key, V value) throws TreeException {
+        if (node == null) {
+            return new AvlLeaf<K,V>(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if ( cmp < 0)
+            node.left  = insert(node.left, key, value);
         else
-            return node;
+            node.right = insert(node.right, key, value);
 
-        updateHeight(node);
+        /* 2. Update height of this ancestor node */
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
 
+        /* 3. Get the balance factor of this ancestor node to check whether
+           this node became unbalanced */
         int balance = getBalance(node);
-        int cmp_left = 0;
-        if (node.left != null)
-            cmp_left = key.compareTo((K) node.left.key);
-        int cmp_right = 0;
 
-        if (node.right !=null)
-            cmp_right = key.compareTo((K) node.right.key);
+        // If this node becomes unbalanced, then there are 4 cases
 
-        if (balance >  1 && cmp_left  < 0) return rightRotate(node);
-        if (balance < -1 && cmp_right > 0) return leftRotate(node);
+        // Left Left Case
+        if (balance > 1 && key.compareTo(node.left.key) < 0)
+            return rightRotate(node);
 
-        if (balance >  1 && cmp_left  > 0) {
-            node.left = leftRotate(node.left);
+        // Right Right Case
+        if (balance < -1 && key.compareTo(node.right.key)>0)
+            return leftRotate(node);
+
+        // Left Right Case
+        if (balance > 1 && key.compareTo(node.left.key) > 0)
+        {
+            node.left =  leftRotate(node.left);
             return rightRotate(node);
         }
 
-        if (balance < -1 && cmp_right < 0) {
+        // Right Left Case
+        if (balance < -1 && key.compareTo(node.right.key) < 0)
+        {
             node.right = rightRotate(node.right);
             return leftRotate(node);
         }
 
+        /* return the (unchanged) node pointer */
         return node;
+    }
+
+    private AvlLeaf rightRotate(AvlLeaf y) {
+        AvlLeaf x = y.left;
+        AvlLeaf T2 = x.right;
+
+        // Perform rotation
+        x.right = y;
+        y.left = T2;
+
+        // Update heights
+        y.height = Math.max(getHeight(y.left), getHeight(y.right))+1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right))+1;
+
+        // Return new root
+        return x;
+    }
+
+    private AvlLeaf leftRotate(AvlLeaf x) {
+        AvlLeaf y = x.right;
+        AvlLeaf T2 = y.left;
+
+        // Perform rotation
+        y.left = x;
+        x.right = T2;
+
+        //  Update heights
+        x.height = Math.max(getHeight(x.left), getHeight(x.right))+1;
+        y.height = Math.max(getHeight(y.left), getHeight(y.right))+1;
+
+        // Return new root
+        return y;
     }
 
     public void delete(K key) {
@@ -143,60 +207,106 @@ public class AvlTree<K extends Comparable<K>, V>  {
     }
 
     AvlLeaf delete(AvlLeaf node, K key) {
-        int cmp = node.key.compareTo(key);
-        if (node == null) {
+        int cmp = 0;
+        // STEP 1: PERFORM STANDARD BST DELETE
+        //System.out.println("key=" + key);
+        if (node == null)
             return node;
-        } else if (cmp > 0) {
+
+        // If the value to be deleted is smaller than the root's value,
+        // then it lies in left subtree
+        cmp = key.compareTo((K)node.key);
+        if ( cmp < 0 )
             node.left = delete(node.left, key);
-        } else if (cmp < 0) {
+
+            // If the value to be deleted is greater than the root's value,
+            // then it lies in right subtree
+        else if( cmp > 0 )
             node.right = delete(node.right, key);
-        } else {
-            if (node.left == null || node.right == null) {
-                node = (node.left == null) ? node.right : node.left;
-            } else {
-                AvlLeaf mostLeftChild = mostLeftChild(node.right);
-                node.key = mostLeftChild.key;
-                node.right = delete(node.right, (K) node.key);
+
+            // if value is same as root's value, then This is the node
+            // to be deleted
+        else {
+            // node with only one child or no child
+            if( (node.left == null) || (node.right == null) ) {
+
+                AvlLeaf temp;
+                if (node.left != null)
+                    temp = node.left;
+                else
+                    temp = node.right;
+
+                // No child case
+                if(temp == null) {
+                    temp = node;
+                    node = null;
+                }
+                else // One child case
+                    node = temp; // Copy the contents of the non-empty child
+
+                temp = null;
+            }
+            else {
+                // node with two children: Get the inorder successor (smallest
+                // in the right subtree)
+                AvlLeaf temp = minValueNode(node.right);
+
+                // Copy the inorder successor's data to this node
+                node.key = temp.key;
+                node.value = temp.value;
+
+                // Delete the inorder successor
+                node.right = delete(node.right, (K) temp.key);
             }
         }
-        if (node != null) {
-            node = rebalance(node);
+
+        // If the tree had only one node then return
+        if (node == null)
+            return node;
+
+        // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+
+        // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to check whether
+        //  this node became unbalanced)
+        int balance = getBalance(node);
+
+        // If this node becomes unbalanced, then there are 4 cases
+
+        // Left Left Case
+        if (balance > 1 && getBalance(node.left) >= 0)
+            return rightRotate(node);
+
+        // Left Right Case
+        if (balance > 1 && getBalance(node.left) < 0) {
+            node.left =  leftRotate(node.left);
+            return rightRotate(node);
         }
+
+        // Right Right Case
+        if (balance < -1 && getBalance(node.right) <= 0)
+            return leftRotate(node);
+
+        // Right Left Case
+        if (balance < -1 && getBalance(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
         return node;
     }
 
-    private AvlLeaf mostLeftChild(AvlLeaf node) {
+    private AvlLeaf minValueNode(AvlLeaf node) {
         AvlLeaf current = node;
         /* loop down to find the leftmost leaf */
-        while (current.left != null) {
+        while (current.left != null)
             current = current.left;
-        }
         return current;
     }
 
-    private AvlLeaf rebalance(AvlLeaf z) {
-        updateHeight(z);
-        int balance = getBalance(z);
-        if (balance > 1) {
-            if (getHeight(z.right.right) > getHeight(z.right.left)) {
-                z = leftRotate(z);
-            } else {
-                z.right = rightRotate(z.right);
-                z = leftRotate(z);
-            }
-        } else if (balance < -1) {
-            if (getHeight(z.left.left) > getHeight(z.left.right)) {
-                z = rightRotate(z);
-            } else {
-                z.left = leftRotate(z.left);
-                z = rightRotate(z);
-            }
-        }
-        return z;
-    }
 
-    private void updateHeight(AvlLeaf n) {
-        n.height = 1 + Math.max(getHeight(n.left), getHeight(n.right));
+    private void updateHeight(AvlLeaf leaf) {
+        leaf.height = Math.max(getHeight(leaf.left), getHeight(leaf.right)) + 1;
     }
 
     AvlLeaf findinner(K key) {
@@ -211,14 +321,16 @@ public class AvlTree<K extends Comparable<K>, V>  {
     }
 
     public V find(K key) {
-        AvlLeaf node = findinner(key);
-        return (V)node.value;
+        if (root == null)
+            return null;
+        AvlLeaf found = root.find(key);
+        return found.key.compareTo(key) == 0 ? (V)found.value : null;
     }
 
     public void change(K oldKey, K newKey) {
         AvlLeaf node =  findinner(oldKey);
         node.key = newKey;
-        rebalance(node);
+
     }
 
     public void process(Consumer<AvlLeaf<K,V>> consumer) {
@@ -234,8 +346,92 @@ public class AvlTree<K extends Comparable<K>, V>  {
         }
     }
 
-    public static void main(String[] args) {
-        AvlTree<Integer, String> tree = new AvlTree();
+    public void print(AvlLeaf root) {
+
+        if (root == null) {
+            System.out.println("(XXXXXX)");
+            return;
+        }
+
+        int height = root.height,
+                width = (int) Math.pow(2, height - 1);
+
+        // Preparing variables for loop.
+        List<AvlLeaf> current = new ArrayList<AvlLeaf>(1),
+                next = new ArrayList<AvlLeaf>(2);
+        current.add(root);
+
+        final int maxHalfLength = 4;
+        int elements = 1;
+
+        StringBuilder sb = new StringBuilder(maxHalfLength * width);
+        for (int i = 0; i < maxHalfLength * width; i++)
+            sb.append(' ');
+        String textBuffer;
+
+        // Iterating through height levels.
+        for (int i = 0; i < height; i++) {
+
+            sb.setLength(maxHalfLength * ((int) Math.pow(2, height - 1 - i) - 1));
+
+            // Creating spacer space indicator.
+            textBuffer = sb.toString();
+
+            // Print tree node elements
+            for (AvlLeaf n : current) {
+
+                System.out.print(textBuffer);
+
+                if (n == null) {
+
+                    System.out.print("        ");
+                    next.add(null);
+                    next.add(null);
+
+                } else {
+
+                    System.out.printf("(%6d)", n.key);
+                    next.add(n.left);
+                    next.add(n.right);
+
+                }
+
+                System.out.print(textBuffer);
+
+            }
+
+            System.out.println();
+            // Print tree node extensions for next level.
+            if (i < height - 1) {
+
+                for (AvlLeaf n : current) {
+
+                    System.out.print(textBuffer);
+
+                    if (n == null)
+                        System.out.print("        ");
+                    else
+                        System.out.printf("%s      %s",
+                                n.left == null ? " " : "/", n.right == null ? " " : "\\");
+
+                    System.out.print(textBuffer);
+
+                }
+
+                System.out.println();
+
+            }
+
+            // Renewing indicators for next run.
+            elements *= 2;
+            current = next;
+            next = new ArrayList<AvlLeaf>(elements);
+
+        }
+    }
+
+    public static void main(String[] args) throws TreeException {
+        /*AvlTree<Integer, String> tree = new AvlTree();
         tree.put(10, "");
         tree.put(20, "");
         tree.put(30, "");
@@ -243,6 +439,35 @@ public class AvlTree<K extends Comparable<K>, V>  {
         tree.put(50, "");
         tree.put(25, "");
         tree.preOrder(tree.root);
-    }
+        System.out.println();
+        //tree.delete(30);
+        tree.preOrder(tree.root);*/
+            AvlTree<Integer, String> t = new AvlTree();
+            //AvlLeaf root = null;
+            while (true) {
+                System.out.println("(1) Insert");
+                System.out.println("(2) Delete");
+
+                try {
+                    BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+                    String s = bufferRead.readLine();
+
+                    if (Integer.parseInt(s) == 1) {
+                        System.out.print("Value to be inserted: ");
+                        t.put(Integer.parseInt(bufferRead.readLine()), "");
+                    } else if (Integer.parseInt(s) == 2) {
+                        System.out.print("Value to be deleted: ");
+                        t.root = t.delete(t.root, Integer.parseInt(bufferRead.readLine()));
+                    } else {
+                        System.out.println("Invalid choice, try again!");
+                        continue;
+                    }
+
+                    t.print(t.root);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
 }
